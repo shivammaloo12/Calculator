981d5baa7196eebb75a57d651b4dbeaa2bd0f1ad
Shivam <shivammaloo12@gmail.com>
2021-05-11 14:48:57 +0530

    Button Function Added

--- app/src/main/java/com/shivam/calculator/MainActivity.kt
+++ app/src/main/java/com/shivam/calculator/MainActivity.kt
@@ -2,10 +2,31 @@ package com.shivam.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun funClear(view: View) {}
+    fun funDivide(view: View) {}
+    fun funMultiply(view: View) {}
+    fun funBackSpace(view: View) {}
+    fun fun7(view: View) {}
+    fun fun8(view: View) {}
+    fun fun9(view: View) {}
+    fun fun4(view: View) {}
+    fun fun5(view: View) {}
+    fun fun6(view: View) {}
+    fun funSub(view: View) {}
+    fun funAdd(view: View) {}
+    fun fun1(view: View) {}
+    fun fun2(view: View) {}
+    fun fun3(view: View) {}
+    fun funDot(view: View) {}
+    fun fun0(view: View) {}
+    fun funPercent(view: View) {}
+    fun funEquals(view: View) {}
 }
